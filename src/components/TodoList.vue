f467d9a545de7574742af8ac41a1260e85ff6004
rhistoba <fujitakeisound@gmail.com>
2019-10-22 09:40:40 +0900

    TodoListコンポーネントを追加してHomeに反映した

---
+++ src/components/TodoList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="todo-list">
+      <div class="todo-item" v-for="todo in todos" :key="todo.id">
+        <div class="todo-item-title">{{ todo.title }}</div>
+        <div class="todo-item-deadline">{{ todo.deadline }}</div>
+        <div class="todo-item-status">{{ todo.status }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "TodoList",
+  data() {
+    return {
+      todos: [
+        {
+          id: 999,
+          title: "すぐに消す",
+          deadline: "2019-11-1",
+          status: "todo"
+        }
+      ]
+    };
+  }
+});
+</script>
+
+<style scoped lang="scss">
+.todo-list {
+  .todo-item {
+    display: flex;
+    justify-content: space-between;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+
+    .todo-item-title {
+      width: 50%;
+    }
+    .todo-item-deadline {
+      width: 30%;
+    }
+    .todo-item-status {
+      width: 20%;
+    }
+  }
+}
+</style>
